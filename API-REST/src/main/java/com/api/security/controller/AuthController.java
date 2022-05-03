c96db2b9611cec92601c81dbc497c7d344cda84b
manuelpz <manuel.lopez.fernandez97@gmail.com>
2022-05-03 13:26:22 +0200

    Security - BACKEND  creada

--- API-REST/src/main/java/com/api/security/controller/AuthController.java
+++ API-REST/src/main/java/com/api/security/controller/AuthController.java
@@ -1,5 +1,82 @@
 package com.api.security.controller;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.api.security.dto.JwtDto;
+import com.api.security.dto.LoginUsuario;
+import com.api.security.dto.NuevoUsuario;
+import com.api.security.entity.Rol;
+import com.api.security.entity.Usuario;
+import com.api.security.enums.RolNombre;
+import com.api.security.jwt.JwtProvider;
+import com.api.security.service.RolService;
+import com.api.security.service.UsuarioService;
+
+@RestController
+@RequestMapping("/auth")
+@CrossOrigin
 public class AuthController {
 
+	@Autowired
+	PasswordEncoder passwordEncoder;
+	
+	@Autowired
+	AuthenticationManager authenticationManager;
+	
+	@Autowired
+	UsuarioService usuarioService;
+	
+	@Autowired
+	RolService rolService;
+	
+	@Autowired
+	JwtProvider jwtProvider;
+	
+	public ResponseEntity<?> nuevo(@Valid @RequestBody NuevoUsuario nuevoUsuario, BindingResult bindingResult){
+		if(bindingResult.hasErrors())
+			return new ResponseEntity("campos mal puestos o email inválido", HttpStatus.BAD_REQUEST);
+		if(usuarioService.existsByNombreUsuario(nuevoUsuario.getNombreUsuario()))
+				return new ResponseEntity("Este nombre de usuario ya existe", HttpStatus.BAD_REQUEST);
+		if(usuarioService.existsByEmail(nuevoUsuario.getEmail()))
+			return new ResponseEntity("Este está registrado", HttpStatus.BAD_REQUEST);
+		Usuario usuario = new Usuario(nuevoUsuario.getNombre(), nuevoUsuario.getNombreUsuario(), nuevoUsuario.getEmail(), passwordEncoder.encode(nuevoUsuario.getPassword()));
+		Set<Rol> roles = new HashSet<>();
+		roles.add(rolService.getByRolNombre(RolNombre.ROLE_USER).get());
+		if(nuevoUsuario.getRoles().contains("admin"))
+			roles.add(rolService.getByRolNombre(RolNombre.ROLE_ADMIN).get());
+		usuario.setRoles(roles);
+		usuarioService.save(usuario);
+		return new ResponseEntity<>("Usuario guardado", HttpStatus.CREATED);	
+	}
+	
+	@PostMapping("/login")
+	public ResponseEntity<JwtDto> login(@Valid @RequestBody LoginUsuario loginUsuario, BindingResult bindingResult){
+		if(bindingResult.hasErrors())
+			return new ResponseEntity("campos mal puestos", HttpStatus.BAD_REQUEST);
+		Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginUsuario.getNombreUsuario(), loginUsuario.getPassword()));
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		String jwt = jwtProvider.generateToken(authentication);
+		UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+		JwtDto jwtDto = new JwtDto(jwt, userDetails.getUsername(), userDetails.getAuthorities());
+		return new ResponseEntity(jwtDto, HttpStatus.OK);
+	}
 }
--- API-REST/src/main/java/com/api/security/repository/UsuarioRepository.java
+++ API-REST/src/main/java/com/api/security/repository/UsuarioRepository.java
@@ -1,5 +1,7 @@
 package com.api.security.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.api.security.entity.Usuario;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer>{
-
+	Optional <Usuario> findByNombreUsuario(String nombreUsuario);
+	boolean existsByNombreUusario(String nombreUsuario);
+	boolean existsByEmail(String email);
 }
