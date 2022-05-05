4d4f74ce1bd27599a0d6e7d5542521a31ffdf339
pedro <pedrokaskis@gmail.com>
2022-05-05 16:47:58 +0200

    no message

--- front-endTFG/src/app/filtro-tipos.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FiltroTiposPipe } from './filtro-tipos.pipe';
-
-describe('FiltroTiposPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FiltroTiposPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
