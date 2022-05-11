1547af4880f9b26f5169e1087e94ddf98e8b24af
manuelpz <manuel.lopez.fernandez97@gmail.com>
2022-05-11 15:00:09 +0200

    nuevo develop2

--- front-endTFG/src/app/dispositivo/nuevo-dispositivo/nuevo-dispositivo.component.ts
+++ front-endTFG/src/app/dispositivo/nuevo-dispositivo/nuevo-dispositivo.component.ts
@@ -49,7 +49,7 @@ export class NuevoDispositivoComponent implements OnInit {
   }
 
   crear(): void {
-    //nuevo develop
+    //nuevo develop2
     const dispositivo = new Dispositivo(this.tipo);
     this.dispositivoService.guardar(dispositivo).subscribe(
       data =>{
